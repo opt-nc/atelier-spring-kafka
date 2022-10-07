04c84889b1d4b77c5b294be510de0265f3222db1
Michèle BARRE <michele.barre@opt.nc>
2022-10-07 14:28:15 +1100

    fix: alignement conf producer kafka ref: #26

--- src/main/java/nc/opt/springkafka/SpringKafkaApplication.java
+++ src/main/java/nc/opt/springkafka/SpringKafkaApplication.java
@@ -2,6 +2,7 @@ package nc.opt.springkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
--- src/main/java/nc/opt/springkafka/config/KafkaStreamConfig.java
+++ src/main/java/nc/opt/springkafka/config/KafkaStreamConfig.java
@@ -1,42 +1,13 @@
 package nc.opt.springkafka.config;
 
-import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.autoconfigure.kafka.StreamsBuilderFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
-import org.springframework.kafka.config.StreamsBuilderFactoryBeanCustomizer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
-@EnableKafka
 @EnableKafkaStreams
 public class KafkaStreamConfig {
-
-    @Autowired
-    private KafkaProperties kafkaProperties;
-
-    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    public KafkaStreamsConfiguration kStreamsConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "smsApp");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class.getName());
-        return new KafkaStreamsConfiguration(props);
-    }
 
     @Bean
     public StreamsBuilderFactoryBeanCustomizer customizer() {
